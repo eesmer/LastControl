2ef6999b905c0482fe53a1c401d478a4e9eecd84
Erkan Esmer <erkanesmer@gmail.com>
2021-08-27 17:23:35 +0300

    Update lastcontrol-installer.sh

--- lastcontrol-installer.sh
+++ lastcontrol-installer.sh
@@ -31,10 +31,12 @@ rm -r /var/www/html/reports && /var/www/html/lastcontrol
 mkdir -p /var/www/html/reports
 mkdir -p /var/www/html/lastcontrol
 cp /root/.ssh/lastcontrol.pub /var/www/html/lastcontrol/
+rm /var/www/html/index.html
 cp /usr/local/lastcontrol/screenshot/index.html /var/www/html/
 cp /usr/local/lastcontrol/screenshot/lastcontrol_logo.png /var/www/html/
 chmod 644 /var/www/html/index.html
 chmod 644 /var/www/html/lastcontrol_logo.png
+systemctl restart apache2.service
 
 touch /usr/local/lastcontrol/hostlist
 
