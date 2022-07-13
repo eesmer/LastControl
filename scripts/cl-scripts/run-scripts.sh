3fcedbe7b8a489960db48d190c29479dffb8dd7b
erkan.esmer <erkanesmer@gmail.com>
2022-07-13 10:42:55 +0300

    php dir removed

---
+++ scripts/cl-scripts/run-scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------
+# Lastcontrol, it does not change any settings in the target system.
+# It just checks and outputs.
+# However, it is your responsibility to run it on any system.
+#----------------------------------------------------------------------
+
+WDIR=/tmp/cl-scripts
+
+chmod +x $WDIR/*
+run-parts $WDIR
