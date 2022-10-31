e96f41f1d20cde96de6c4280aafc237204ea85a7
erkan.esmer <erkanesmer@gmail.com>
2022-10-31 19:09:23 +0300

    run-parts script updated

--- install/machine/usr/local/lastcontrol/scripts/cl-scripts/run-scripts.sh
+++ install/machine/usr/local/lastcontrol/scripts/cl-scripts/run-scripts.sh
@@ -6,10 +6,20 @@
 # However, it is your responsibility to run it on any system.
 #----------------------------------------------------------------------
 
-#WDIR=/tmp/cl-scripts
 WDIR=/usr/local/cl-scripts
 
-chmod +x $WDIR/*
-run-parts $WDIR
-
-#rm -r $WDIR
+bash $WDIR/01-create-inventory
+bash $WDIR/02-create-systemreport
+bash $WDIR/03-create-updatereport
+bash $WDIR/04-create-diskreport
+bash $WDIR/05-create-kernelreport
+bash $WDIR/06-create-nwconfigreport
+bash $WDIR/07-create-grubreport
+bash $WDIR/08-create-servicereport
+bash $WDIR/09-create-unsecurepackreport
+bash $WDIR/10-create-firewallreport
+bash $WDIR/11-create-localuserreport
+bash $WDIR/12-create-suidsgidreport
+bash $WDIR/13-create-vulnerabilityreport
+bash $WDIR/14-create-sshreport
+bash $WDIR/15-create-directoryreport
