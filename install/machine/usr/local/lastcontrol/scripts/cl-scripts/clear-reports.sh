afd89bc948783252a8ed4bafe770e95a1a134baf
erkan.esmer <erkanesmer@gmail.com>
2023-02-19 05:01:38 +0300

    role control updated

---
+++ install/machine/usr/local/lastcontrol/scripts/cl-scripts/clear-reports.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#----------------------------------------------------------------------
+# Lastcontrol, it does not change any settings in the target system.
+# It just checks and outputs.
+# However, it is your responsibility to run it on any system.
+#----------------------------------------------------------------------
+
+WDIR=/usr/local/cl-scripts
+RDIR=/usr/local/lastcontrol-reports
+
+rm -r $WDIR
+rm -r $RDIR
