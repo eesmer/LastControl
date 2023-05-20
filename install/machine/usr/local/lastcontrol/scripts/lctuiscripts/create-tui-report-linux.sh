bbd7e49eeae5e49335f385ce21928940622146b1
erkan.esmer <erkanesmer@gmail.com>
2023-05-20 15:41:16 +0300

    tui scripts updated

--- install/machine/usr/local/lastcontrol/scripts/lctuiscripts/create-tui-report-linux.sh
+++ install/machine/usr/local/lastcontrol/scripts/lctuiscripts/create-tui-report-linux.sh
@@ -2,45 +2,19 @@
 
 WDIR=/usr/local/lastcontrol
 RDIR=/usr/local/lastcontrolreports
-LCSCRIPTS=$WDIR/scripts/lctuiscripts
+SCRIPTS=$WDIR/scripts
+TUISCRIPTS=lctuiscripts
 LCKEY=/root/.ssh/lastcontrol
-###LCSCRIPTPATH=/usr/local/lcscripts
-###LCREPORTPATH=/usr/local/lcreports
 
 rm -r $RDIR/$2
 
-scp -r -P22 -i $LCKEY $LCSCRIPTS root@$2:/usr/local/
-
-exit 1
-
-ssh -p22 -i $LCKEY root@$2 -- bash $LCSCRIPTS/$1.sh
+scp -r -P22 -i $LCKEY $SCRIPTS/$TUISCRIPTS root@$2:/usr/local/
+ssh -p22 -i $LCKEY root@$2 -- bash /usr/local/$TUISCRIPTS/$1.sh
 
 mkdir -p $RDIR/$2
-scp -r -P22 -i $LCKEY root@$2:$LCSCRIPTS/$2/$2-$1.txt $RDIR
-scp -r -P22 -i $LCKEY root@$2:$LCSCRIPTS/$2/$2-$1.md $RDIR
+scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.txt $RDIR
+scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.md $RDIR
 
 # copying report attachments
-scp -r -P22 -i $LCKEY root@$2:$LCSCRIPTS/$2/$2-listeningservice.txt $RDIR
-scp -r -P22 -i $LCKEY root@$2:$LCSCRIPTS/$2/$2-establishedservice.txt $RDIR
-
-##echo "" >> $RDIR/$2-$1.md
-##echo "![](/tmp/lastcontrol_logo.png){ width=25% }" >> $RDIR/$2-$1.md
-
-#echo ""
-#cat $RDIR/$2-$1.txt
-
-#NUMMACHINE=$(cat $WDIR/linuxhost | wc -l)
-#i=1
-#while [ "$i" -le "$NUMMACHINE" ]; do
-#	LINUXMACHINE=$(ls -l |sed -n $i{p} $WDIR/linuxhost)
-#	
-#	scp -r -P22 -i $LCKEY $CLSCRIPTS root@$LINUXMACHINE:/usr/local/
-#	ssh -p22 -i $LCKEY root@$LINUXMACHINE -- bash /usr/local/cl-scripts/run-scripts.sh
-#
-#	mkdir -p $RDIR/$LINUXMACHINE
-#	scp -r -P22 -i $LCKEY root@$LINUXMACHINE:/usr/local/lastcontrol-reports/$LINUXMACHINE  /usr/local/lastcontrolreports/
-#
-#	ssh -p22 -i $LCKEY root@$LINUXMACHINE -- bash /usr/local/cl-scripts/clear-reports.sh
-#
-#i=$(( i + 1 ))
-#done
+scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-listeningservice.txt $RDIR
+scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-establishedservice.txt $RDIR
