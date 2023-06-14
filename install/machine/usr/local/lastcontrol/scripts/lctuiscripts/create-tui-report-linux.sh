e6c0b6262d5c838801f5c1e9000c1dfd1736ccbe
erkan.esmer <erkanesmer@gmail.com>
2023-06-14 09:09:43 +0300

    create-tui-report script edited

--- install/machine/usr/local/lastcontrol/scripts/lctuiscripts/create-tui-report-linux.sh
+++ install/machine/usr/local/lastcontrol/scripts/lctuiscripts/create-tui-report-linux.sh
@@ -9,17 +9,17 @@ BOARDFILE=/usr/local/lastcontrol/doc/board.txt
 
 rm -r $RDIR/$2*
 
-scp -r -P22 -i $LCKEY $SCRIPTS/$TUISCRIPTS root@$2:/usr/local/
-ssh -p22 -i $LCKEY root@$2 -- bash /usr/local/$TUISCRIPTS/$1.sh
+scp -r -P22 -i $LCKEY $SCRIPTS/$TUISCRIPTS root@$2:/usr/local/ &> /dev/null
+ssh -p22 -i $LCKEY root@$2 -- bash /usr/local/$TUISCRIPTS/$1.sh &> /dev/null
 
 mkdir -p $RDIR/$2
-scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.txt $RDIR
-scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.md $RDIR
+scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.txt $RDIR &> /dev/null
+scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.md $RDIR &> /dev/null
 
 # copying report attachments
 if [ "$1" = servicereport ]; then
-	scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-listeningservice.txt $RDIR
-	scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-establishedservice.txt $RDIR
+	scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-listeningservice.txt $RDIR &> /dev/null
+	scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-establishedservice.txt $RDIR &> /dev/null
 fi
 
 echo "Info: $1 generated." > $BOARDFILE
