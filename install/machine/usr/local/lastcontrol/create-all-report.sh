c29a5d8b97e3cea8d08b9fc98c02bca99b12907e
erkan.esmer <erkanesmer@gmail.com>
2023-08-25 20:16:40 +0300

    server dir design changed

--- install/machine/usr/local/lastcontrol/create-all-report.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WDIR=/usr/local/lastcontrol
-
-chmod +x $WDIR/scripts/*
-run-parts $WDIR/scripts
--- lastcontrol/scripts/linux/create-report-linux.sh
+++ lastcontrol/scripts/linux/create-report-linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WDIR=/usr/local/lastcontrol
-RDIR=/usr/local/lastcontrolreports
+RDIR=$WDIR/reports
 SCRIPTS=$WDIR/scripts
 TUISCRIPTS=lctuiscripts
 LCKEY=/root/.ssh/lastcontrol
@@ -13,13 +13,13 @@ scp -r -P22 -i $LCKEY $SCRIPTS/$TUISCRIPTS root@$2:/usr/local/ &> /dev/null
 ssh -p22 -i $LCKEY root@$2 -- bash /usr/local/$TUISCRIPTS/$1.sh &> /dev/null
 
 mkdir -p $RDIR/$2
-scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.txt $RDIR &> /dev/null
-scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.md $RDIR &> /dev/null
+scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.txt $RDIR/$2 &> /dev/null
+scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-$1.md $RDIR/$2 &> /dev/null
 
 # copying report attachments
 if [ "$1" = servicereport ]; then
-	scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-listeningservice.txt $RDIR &> /dev/null
-	scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-establishedservice.txt $RDIR &> /dev/null
+	scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-listeningservice.txt $RDIR/$2 &> /dev/null
+	scp -r -P22 -i $LCKEY root@$2:/usr/local/lcreports/$2/$2-establishedservice.txt $RDIR/$2 &> /dev/null
 fi
 
 echo "Info: $1 generated." > $BOARDFILE
