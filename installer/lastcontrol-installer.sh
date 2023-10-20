0bf49730426dd34fd7a50c2c1e54b200b110aa33
erkan.esmer <erkanesmer@gmail.com>
2023-10-20 06:18:39 +0300

    lastcontrol installer updated

--- installer/lastcontrol-installer.sh
+++ installer/lastcontrol-installer.sh
@@ -84,6 +84,7 @@ function new_install(){
 	  apt-get -y install xsltproc
 	  apt-get -y install imagemagick
 	  apt-get -y install pandoc texlive-latex-base texlive-fonts-recommended
+      apt-get -y install texlive-latex-recommended
 	  #apt-get -y install sqlite3
 	  ##apt-get -y install php
 	  ##apt-get -y install php-sqlite3
