c0ccbec9ad132ec0c972dba1d7f2e636bf58191c
erkan.esmer <erkanesmer@gmail.com>
2023-11-20 21:17:03 +0300

    add allreport.sh script

--- lastcontrol/scripts/linux/allreport.sh
+++ lastcontrol/scripts/linux/allreport.sh
@@ -14,4 +14,16 @@ $HOST_NAME LastControl Report $DATE
 |IP Address:        |$INT_IPADDR | $EXT_IPADDR
 |Internet Conn.     |$INTERNET   | Installation Check: $INSTALL_CHECK
 --------------------------------------------------------------------------------------------------------------------------
-
+|CPU Info:          |$CPUINFO
+|RAM:               |Total:$RAM_TOTAL | Usage:$RAM_USAGE
+|VGA Controller:    |$VGA_CONTROLLER
+|HDD:               |$DISK_LIST
+|Virtualization:    |$VIRT_CONTROL
+|Operation System:  |$OS_KERNEL
+|OS Version:        |$OS_VER
+|Check Update:      |$CHECK_UPDATE
+|Update Count:      |$SYSUPDATE_COUNT
+|Last Boot:         |$LAST_BOOT
+|Uptime             |$UPTIME | $UPTIME_MIN
+|Kernel Version:    |$OS_KERNEL
+--------------------------------------------------------------------------------------------------------------------------
