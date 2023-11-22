5101038b549b676d60cca56fe284d7c80db27bfb
erkan.esmer <erkanesmer@gmail.com>
2023-11-22 23:05:43 +0300

    aalreport.sh edited

--- lastcontrol/scripts/linux/allreport.sh
+++ lastcontrol/scripts/linux/allreport.sh
@@ -22,6 +22,35 @@ grep -i "red hat" $RDIR/distrocheck &> /dev/null && REP=YUM
 #cat /etc/*-release | grep "NAME=" | grep -v "PRETTY_NAME" | grep -v "CPE_NAME" | cut -d "=" -f2
 rm $RDIR/distrocheck
 
+DATE=$(date)
+HOST_NAME=$(cat /etc/hostname)
+
+#----------------------------
+# Inventory
+#----------------------------
+INT_IPADDR=$(hostname -I)
+EXT_IPADDR=$(curl -4 icanhazip.com)
+CPUINFO=$(cat /proc/cpuinfo |grep "model name" |cut -d ':' -f2 > /tmp/cpuinfooutput.txt && tail -n1 /tmp/cpuinfooutput.txt > /tmp/cpuinfo.txt && rm /tmp/cpuinfooutput.txt && cat /tmp/cpuinfo.txt) && rm /tmp/cpuinfo.txt
+RAM_TOTAL=$(free -m | head -2 | tail -1| awk '{print $2}')
+RAM_USAGE=$(free -m | head -2 | tail -1| awk '{print $3}')
+GPU=$(lspci | grep VGA | cut -d ":" -f3);GPURAM=$(cardid=$(lspci | grep VGA |cut -d " " -f1);lspci -v -s $cardid | grep " prefetchable"| awk '{print $6}' | head -1)
+VGA_CONTROLLER="'$GPU' '$GPURAM'"
+##################DISK_LIST=$(df -lh | grep -vE 'Filesystem|tmpfs|cdrom|udev' | awk '{ print $5" "$1"("$2"  "$3")" " --- "}' | sed -e :a -e N -e 's/\n/ /' -e ta)
+DISK_LIST=$(fdisk -lu | grep "Disk" | grep -v "Disklabel" | grep -v "dev/loop" | grep -v "Disk identifier" | cut -d ":" -f1)
+VIRT_CONTROL=NONE
+if [ -f "/dev/kvm" ]; then "$VIRT_CONTROL"=ON; fi
+OS_KERNEL=$(uname -mrsv)
+OS_VER=$(cat /etc/os-release |grep PRETTY_NAME | cut -d '=' -f2 |cut -d '"' -f2)
+LAST_BOOT=$(who -b | awk '{print $3,$4}')
+UPTIME=$(uptime) && UPTIME_MIN=$(awk '{ print "up " $1 /60 " minutes"}' /proc/uptime)
+ping -c 1 google.com &> /dev/null && INTERNET="CONNECTED" || INTERNET="DISCONNECTED"
+DISTRO=$(cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 |cut -d '"' -f2)
+KERNEL=$(uname -mrs)
+TIMEZONE=$(timedatectl | grep "Time zone:" | cut -d ":" -f2 | xargs)
+LOCALDATE=$(timedatectl | grep "Local time:" | awk '{print $3,$4,$5}')
+LASTBOOT=$(who -b | awk '{print $3,$4}')
+UPTIME=$(uptime | awk '{print $1,$2,$3,$4}' |cut -d "," -f1)
+
 ######################
 # Create TXT Report File
 ######################
