3c2b2cfdb6cfce436f26a9ba55fd93db96af576a
erkan.esmer <erkanesmer@gmail.com>
2024-04-19 22:02:47 +0300

    lastcontrol.sh edited

--- lastcontrol.sh
+++ lastcontrol.sh
@@ -15,8 +15,8 @@ Options:
   --help, -h		Show this help message
   --report-localhost	It checks the server (local machine) you are running on
   --report-remotehost	It checks the remote server
+  --report-allhost	Generates reports from all remote servers in Host List
   --server-install      Installs LastControl Server to perform remote server control
-  --report-all-host	Generates reports from all remote servers in Host List
   --add-host            LastControl SSH Key is added to the server and included in the Host List
   --remove-host		LastContol SSH Key is deleted and removed from the Host list
   --host-list		List of Added Hosts
@@ -600,7 +600,6 @@ if [ "$1" = "--report-allhost" ]; then
 		HOST=$(ls -l | sed -n $i{p} $WDIR/linuxmachine)
 		HNAME=$(echo $HOST | cut -d " " -f1)
 		PNAME=$(echo $HOST | cut -d " " -f2)
-		
 		$BOLD
 		$YELLOW
 		echo -e
