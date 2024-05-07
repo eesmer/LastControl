78b4c713c209f696c29d3bf1fcbb7ff92b37bd1e
erkan.esmer <erkanesmer@gmail.com>
2024-05-07 10:49:56 +0300

    lastcontrol.sh updated for audit commands

--- lastcontrol.sh
+++ lastcontrol.sh
@@ -424,6 +424,17 @@ SERVICE_PROCESS(){
 	ZO_PROCESS=$(ps -A -ostat,ppid,pid,cmd | grep -e '^[Zz]' | wc -l)
 }
 
+AUDIT() {
+	#MOST COMMANDS
+	HISTORY_FILE=~/.bash_history
+	if [ -f "$HISTORY_FILE" ]; then
+		#MOST_COMMANDS=$(cat ~/.bash_history | awk '{cmd[$2]++} END {for(elem in cmd) {print cmd[elem] " " elem}}' | sort -n -r | head -20 | cut -d " " -f2 | paste -sd ",")
+		MOST_COMMANDS=$(cat $HISTORY_FILE | head -10 | paste -sd ",")
+	else
+		MOST_COMMANDS="bash_history file not found in ~/ directory"
+	fi
+}
+
 MOST_COMMANDS() {
 	HISTORY_FILE=~/.bash_history
 	if [ -f "$HISTORY_FILE" ]; then
