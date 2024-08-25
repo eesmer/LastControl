b8901601b4b8e7caa900877b7d1290f6e1c1fddf
erkan.esmer <erkanesmer@gmail.com>
2024-08-25 10:06:05 +0300

    lastcontrol.sh edited

--- lastcontrol.sh
+++ lastcontrol.sh
@@ -65,21 +65,6 @@ EOF
 echo -e
 }
 
-#------------------
-# Color Codes
-#------------------
-MAGENTA="tput setaf 1"
-GREEN="tput setaf 2"
-YELLOW="tput setaf 3"
-DGREEN="tput setaf 4"
-CYAN="tput setaf 6"
-WHITE="tput setaf 7"
-GRAY="tput setaf 8"
-RED="tput setaf 9"
-BLUE="tput setaf 12"
-NOCOL="tput sgr0"
-BOLD="tput bold"
-	
 HOST_NAME=$(cat /etc/hostname)
 WDIR=/usr/local/lastcontrol
 RDIR=/usr/local/lastcontrol/reports
