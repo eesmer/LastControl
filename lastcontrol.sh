29d4c64c1c0d4ed18e21b592e722698f3717c4f9
erkan.esmer <erkanesmer@gmail.com>
2024-09-13 12:10:10 +0300

    lastcontrol.sh edited

--- lastcontrol.sh
+++ lastcontrol.sh
@@ -159,22 +159,22 @@ CHECK_UPDATE() {
 	echo "Checking for Updates"
 	$NOCOL
 	if [ "$REP" = "APT" ]; then
-      UPDATE_OUTPUT=$(apt update 2>&1)
-      if echo "$UPDATE_OUTPUT" | grep -qE "(Failed to fetch|Temporary failure resolving|Could not resolve|Some index files failed to download)"; then
-          UPDATE_COUNT="Some errors occurred during apt update. Please check internet or repository access."
-      else
-          UPDATE_COUNT=$(apt list --upgradable | grep -v "Listing" | wc -l)
-      fi
+		UPDATE_OUTPUT=$(apt update 2>&1)
+		if echo "$UPDATE_OUTPUT" | grep -qE "(Failed to fetch|Temporary failure resolving|Could not resolve|Some index files failed to download)"; then
+			UPDATE_COUNT="Some errors occurred during apt update. Please check internet or repository access."
+		else
+			UPDATE_COUNT=$(apt list --upgradable 2>&1 | grep -v "Listing" | wc -l)
+		fi
 	fi
 	if [ "$REP" = "YUM" ]; then
-      UPDATE_OUTPUT=$(yum update -y 2>&1)
-      if echo "$UPDATE_OUTPUT" | grep -qE "(Failed to download|Could not resolve host|Temporary failure in name resolution|No more mirrors to try)"; then
-          UPDATE_COUNT="Some errors occurred during yum update. Please check internet or repository access."
-      else
-          UPDATE_COUNT=$(echo N | yum update | grep "Upgrade" | awk '{print $2}')
-          INSTALL_COUNT=$(echo N | yum update | grep "Install" | grep -v "Installing"| awk '{print $2}')
-          UPDATE_COUNT=$(expr $UPDATE_COUNT + $INSTALL_COUNT)
-      fi
+		UPDATE_OUTPUT=$(yum update -y 2>&1)
+		if echo "$UPDATE_OUTPUT" | grep -qE "(Failed to download|Could not resolve host|Temporary failure in name resolution|No more mirrors to try)"; then
+			UPDATE_COUNT="Some errors occurred during yum update. Please check internet or repository access."
+		else
+			UPDATE_COUNT=$(echo N | yum update | grep "Upgrade" | awk '{print $2}')
+			INSTALL_COUNT=$(echo N | yum update | grep "Install" | grep -v "Installing"| awk '{print $2}')
+			UPDATE_COUNT=$(expr $UPDATE_COUNT + $INSTALL_COUNT)
+		fi
 	fi
 }
 
@@ -185,6 +185,9 @@ SYSTEM_REPORT() {
 	mkdir -p $RDIR
 	
 	# LOCAL USERS
+	$BLUE
+        echo "Checking Local User Accounts"
+        $NOCOL
 	LOCALUSERS=$(mktemp)
 	#grep -E "/bin/bash|/bin/zsh|/bin/sh" /etc/passwd | grep -v "/sbin/nologin" | grep -v "/bin/false" | cut -d":" -f1 > $LOCALUSERS
 	cat /etc/shadow | grep -v "*" | grep -v "!" | cut -d ":" -f1 > $LOCALUSERS
@@ -193,6 +196,9 @@ SYSTEM_REPORT() {
 	LOGGED_USERS=$(w -hi)
 	
 	# HARDWARE INVENTORY
+	$BLUE
+        echo "Checking Hardware Inventory"
+        $NOCOL
 	INTERNAL_IP=$(hostname -I | cut -d " " -f1)
 	EXTERNAL_IP=$(curl -4 icanhazip.com 2>/dev/null)
 	CPU_INFO=$(awk -F ':' '/model name/ {print $2}' /proc/cpuinfo | head -n 1 | xargs)
@@ -230,6 +236,9 @@ SYSTEM_REPORT() {
 }
 
 CHECK_QUOTA() {
+	$BLUE
+        echo "Checking Disk Quota Configuration"
+        $NOCOL
 	if command -v quotacheck &> /dev/null; then
 		QUOTA_INSTALL=Pass
 	else
@@ -248,6 +257,9 @@ CHECK_QUOTA() {
 }
 
 LVM_CRYPT() {
+	$BLUE
+        echo "Checking LVM Configuration"
+        $NOCOL
 	if lsblk --output type | grep -qw "lvm"; then
 		LVM_USAGE=Pass
 	else
