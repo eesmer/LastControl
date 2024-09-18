e42edbc508633d52ec19bbd153d0a02f2c347271
erkan.esmer <erkanesmer@gmail.com>
2024-09-18 21:05:03 +0300

    ssh log edited

--- lastcontrol.sh
+++ lastcontrol.sh
@@ -656,7 +656,8 @@ SSH_AUTH_LOGS() {
 	echo "Accepted" >> $RDIR/$HOST_NAME-allreports.txt
 	echo "------------------------------------" >> $RDIR/$HOST_NAME-allreports.txt
 	#find /var/log -type f -exec sh -c "cat {} | egrep -i 'ssh'" \; | grep "Accepted" | tail -n 10 >> $RDIR/$HOST_NAME-allreports.txt
-	find /var/log/ -type f -exec grep -I -i "ssh" {} \; | grep "Accepted" | tail -n 10 >> $RDIR/$HOST_NAME-allreports.txt
+	#find /var/log/ -type f -exec grep -I -i "ssh" {} \; | grep "Accepted" | tail -n 10 >> $RDIR/$HOST_NAME-allreports.txt
+	find /var/log/ -type f -exec sh -c 'file "{}" | grep -q text && grep -i "ssh" "{}"' \; | grep "Accepted" | tail -n 10 >> $RDIR/$HOST_NAME-allreports.txt
 	echo "------------------------------------" >> $RDIR/$HOST_NAME-allreports.txt
 	echo "Failed" >> $RDIR/$HOST_NAME-allreports.txt >> $RDIR/$HOST_NAME-allreports.txt
 	echo "------------------------------------" >> $RDIR/$HOST_NAME-allreports.txt
