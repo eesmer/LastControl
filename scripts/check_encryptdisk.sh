046984e57569bf3ab8518c30e0bc2d5ddd9ce178
erkan.esmer <erkanesmer@gmail.com>
2024-10-16 18:02:11 +0300

    scripts dir updated

--- scripts/check_encryptdisk.sh
+++ scripts/check_encryptdisk.sh
@@ -5,21 +5,22 @@
 # This script checks the encrypted disk for LUKS
 #-------------------------------------------------
 
-ENCRYPTED_DISK=$(lsblk -o NAME,FSTYPE | grep "crypto_LUKS")
-
-ENCRYPT_USAGE=FAIL
-if [[ -n $ENCRYPTED_DISK ]]; then
-	ENCRYPT_USAGE=PASS
-fi
-
-if [[ $ENCRYPTED_USAGE == PASS ]]; then
-	while read -r DISK; do
-		DEVICENAME=$(echo $DISK | awk '{print $1}')
-		echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
-		echo "|ENCRYPTED DISK INFORMATION |" >> $ENCRYPTINFO
-		echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
-		echo "Disk: /dev/$DEVICENAME" >> $ENCRYPTINFO
-		cryptsetup luksDump /dev/$DEVICENAME | grep -E "Version|Cipher name|UUID" >> $ENCRYPTINFO
-		echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
-	done <<< "$ENCRYPTED_DISK"
-fi
+ENCRYPT_DISK() {
+	ENCRYPTED_DISK=$(lsblk -o NAME,FSTYPE | grep "crypto_LUKS")
+	ENCRYPT_USAGE=FAIL
+	if [[ -n $ENCRYPTED_DISK ]]; then
+		ENCRYPT_USAGE=PASS
+	fi
+	
+	if [[ $ENCRYPTED_USAGE == PASS ]]; then
+		while read -r DISK; do
+			DEVICENAME=$(echo $DISK | awk '{print $1}')
+			echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
+			echo "|ENCRYPTED DISK INFORMATION |" >> $ENCRYPTINFO
+			echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
+			echo "Disk: /dev/$DEVICENAME" >> $ENCRYPTINFO
+			cryptsetup luksDump /dev/$DEVICENAME | grep -E "Version|Cipher name|UUID" >> $ENCRYPTINFO
+			echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
+		done <<< "$ENCRYPTED_DISK"
+	fi
+}
