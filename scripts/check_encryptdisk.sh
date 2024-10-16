bc15674f87e1515976bb1d52603b4725ff284136
erkan.esmer <erkanesmer@gmail.com>
2024-10-16 17:58:28 +0300

    encryptdisk.sh added to scripts dir

---
+++ scripts/check_encryptdisk.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#-------------------------------------------------
+# LastControl Check Encrypted Disk
+# This script checks the encrypted disk for LUKS
+#-------------------------------------------------
+
+ENCRYPTED_DISK=$(lsblk -o NAME,FSTYPE | grep "crypto_LUKS")
+
+ENCRYPT_USAGE=FAIL
+if [[ -n $ENCRYPTED_DISK ]]; then
+	ENCRYPT_USAGE=PASS
+fi
+
+if [[ $ENCRYPTED_USAGE == PASS ]]; then
+	while read -r DISK; do
+		DEVICENAME=$(echo $DISK | awk '{print $1}')
+		echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
+		echo "|ENCRYPTED DISK INFORMATION |" >> $ENCRYPTINFO
+		echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
+		echo "Disk: /dev/$DEVICENAME" >> $ENCRYPTINFO
+		cryptsetup luksDump /dev/$DEVICENAME | grep -E "Version|Cipher name|UUID" >> $ENCRYPTINFO
+		echo "--------------------------------------------------------------------------------------------------------------------------" >> $ENCRYPTINFO
+	done <<< "$ENCRYPTED_DISK"
+fi
