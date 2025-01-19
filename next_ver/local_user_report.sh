26021dd0720cf64728220fc85ec50f7d0292a9fe
erkan.esmer <erkanesmer@gmail.com>
2025-01-19 11:51:42 +0300

    local_user_report.sh updated

--- next_ver/local_user_report.sh
+++ next_ver/local_user_report.sh
@@ -48,3 +48,14 @@ check_sudo_permissions() {
     echo -e "\n${CYAN}--- SUDO Permissions for $username ---${RESET}"
     sudo -lU "$username" 2>/dev/null || echo "No SUDO permissions."
 }
+
+# Users in sudoers file
+check_sudoers() {
+    echo -e "\n${YELLOW}--- User Defined in the sudoers file ---${RESET}"
+    for sudo_file in /etc/sudoers.d/*; do
+        if [ -f "$sudo_file" ] && [ "$(basename "$sudo_file")" != "README" ]; then
+            echo -e "${GREEN}File:${RESET} $sudo_file"
+            cat "$sudo_file"
+        fi
+    done
+}
