7bd78c914908e24e534a3213ed089e9333b3a390
erkan.esmer <erkanesmer@gmail.com>
2025-01-24 21:50:27 +0300

    resource_analysis_report.sh edited

--- next_ver/resource_analysis_report.sh
+++ next_ver/resource_analysis_report.sh
@@ -2,3 +2,6 @@ echo "top 5 cpu consuming processes"
 ps -eo pid,comm,%cpu --sort=-%cpu | head -n 6
 echo -e
 
+echo top 5 memory consuming process
+ps -eo pid,comm,%mem --sort=-%mem | head -n 6
+echo -e
