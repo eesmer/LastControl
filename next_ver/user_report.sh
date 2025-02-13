6e9fc05d4caef38b9e2397db5226870542736961
erkan.esmer <erkanesmer@gmail.com>
2025-02-13 21:25:11 +0300

    user_report.sh edited

--- next_ver/user_report.sh
+++ next_ver/user_report.sh
@@ -2,3 +2,16 @@
 
 mkdir -p /usr/local/lastcontrol/reports
 report_file="/usr/local/lastcontrol/reports/user_status.json"
+
+echo "[" > "$output_file"
+
+for user in $(getent passwd | awk -F: '{print $1}'); do
+    shell=$(getent passwd "$user" | cut -d: -f7)
+
+    # User Type
+    if [[ "$shell" != "/usr/sbin/nologin" && "$shell" != "/bin/false" ]]; then
+        user_type="Real User"
+    else
+        user_type="System User"
+    fi
+
