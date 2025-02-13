5b641f1a528649acbf640ab139b0ee11a7e3a3fe
erkan.esmer <erkanesmer@gmail.com>
2025-02-13 21:25:46 +0300

    user_report.sh edited

--- next_ver/user_report.sh
+++ next_ver/user_report.sh
@@ -15,3 +15,21 @@ for user in $(getent passwd | awk -F: '{print $1}'); do
         user_type="System User"
     fi
 
+    # Sudo Check
+    sudo_access="No"
+
+    # SUDO Control: sudo group member, /etc/sudoers.d/ and sudo -lU control
+    if groups "$user" | grep &>/dev/null '\bsudo\b'; then
+        sudo_access="Yes"
+    elif [ -f "/etc/sudoers.d/$user" ]; then
+        sudo_access="Yes"
+    elif sudo -lU "$user" 2>/dev/null | grep -q "(ALL : ALL)"; then
+        sudo_access="Yes"
+    fi
+
+    # Account Expiry Control
+    account_expires=$(chage -l "$user" | grep 'Account expires' | cut -d: -f2 | xargs)
+    if [ -z "$account_expires" ]; then
+        account_expires="Never"
+    fi
+
