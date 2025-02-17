ea81aaeb2779c7adf1f17338b00cdba740a2e833
erkan.esmer <erkanesmer@gmail.com>
2025-02-17 21:07:52 +0300

    update_report web page edited

--- next_ver/web/update_report.php
+++ next_ver/web/update_report.php
@@ -18,3 +18,25 @@ $updates = json_decode($jsonData, true);
         <a href="index.php" class="home-button">Homepage</a>
         <h1>Update Report</h1>
     </div>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Package Name</th>
+                <th>Current Version</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (isset($updates['updates']) && !empty($updates['updates'])): ?>
+                <?php foreach ($updates['updates'] as $update): ?>
+                    <tr>
+                        <td><?= htmlspecialchars($update['package']) ?></td>
+                        <td><?= htmlspecialchars($update['current_version']) ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr><td colspan="2">Update Not Found</td></tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+
