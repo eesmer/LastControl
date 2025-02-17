2beab7f76d6147cae0a9707aed2902cfa5e29294
erkan.esmer <erkanesmer@gmail.com>
2025-02-17 21:06:49 +0300

    update_report web page edited

--- next_ver/web/update_report.php
+++ next_ver/web/update_report.php
@@ -3,3 +3,18 @@ $jsonFile = '/var/www/html/update_report.json';
 $jsonData = file_get_contents($jsonFile);
 $updates = json_decode($jsonData, true);
 ?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Report</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="top-bar">
+        <a href="index.php" class="home-button">Homepage</a>
+        <h1>Update Report</h1>
+    </div>
