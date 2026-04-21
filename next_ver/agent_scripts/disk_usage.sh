d2616b3b04a32fb2ad5fec184ece1a01e1936390
erkan.esmer <erkanesmer@gmail.com>
2026-04-21 15:14:43 +0300

    delete a old and test files

--- next_ver/agent_scripts/disk_usage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DISK_DATA=$(df -h | grep '^/dev/' | awk '{print $1"|"$2"|"$3"|"$5}' | tr '\n' ',' | sed 's/,$//')
-
-jq -c -n \
-  --arg org "system_info_disk" \
-  --arg hn "$(hostname)" \
-  --arg data "$DISK_DATA" \
-  '{origin: $org, hostname: $hn, info_data: $data}'
-
