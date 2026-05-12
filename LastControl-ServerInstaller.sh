ae17c986d22591e4e1ddb6c7f2a498a805ef27f1
erkan.esmer <erkanesmer@gmail.com>
2026-05-12 19:43:20 +0300

    Server Installer script edited for req. packages

--- LastControl-ServerInstaller.sh
+++ LastControl-ServerInstaller.sh
@@ -13,3 +13,14 @@ PORT="4433"
 REPO_URL="https://github.com/eesmer/LastControl.git"
 TEMP_REPO="/tmp/LastControl_Repo"
 
+rm -rf "/etc/lastcontrol"
+rm -rf "$AGENT_DIR"
+mkdir -p "$CERT_DIR" "$AGENT_DIR"
+
+echo "--- LastControl: Installing Required Packages ---"
+echo "--- LastControl: Creating Certificate and Security Infrastructure ---"
+apt update
+apt-get -y install socat jq sqlite3
+apt-get -y install python3-venv python3-pip nginx
+apt-get -y install rsyslog
+
